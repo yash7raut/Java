98dcceea0084dea645c5f145d25ba5ff3c84310f
yash <yash7raut@gmail.com>
2023-02-15 23:12:58 +0530

    leap_year program

---
+++ 01-flow_of_programs/leap_year.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class leap_year{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the year to be checked!!");
+        int year = input.nextInt();
+        if((year%400 == 0) || (year%4 == 0) && (year%100!=0) ){
+            System.out.println(year+" is a leap year!!");
+        }
+        else{
+            System.out.println(year+" is not a leap year!!");
+        }
+        input.close();
+    }
+}
