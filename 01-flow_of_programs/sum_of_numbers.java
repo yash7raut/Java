0dba0d4400e10f0221697f577a5f6c992d37935f
yash <yash7raut@gmail.com>
2023-02-16 22:48:39 +0530

    Multiplication Table

---
+++ 01-flow_of_programs/sum_of_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class sum_of_numbers{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int input1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int input2 = input.nextInt();
+        int result = input1 + input2;
+        System.out.println("THe sum of thwo number is "+result);
+        input.close();
+    }
+}
