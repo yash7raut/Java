3aed7818f3e4718e4c2cc744dc0ab3df43f67f2d
yash <yash7raut@gmail.com>
2023-02-17 17:02:53 +0530

    HCF_LCM

---
+++ 01-flow_of_programs/HCF_.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HCF_{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first Number");
+        int input1 = input.nextInt();
+        System.out.println("Enter the second Number");
+        int input2 = input.nextInt();
+        int result
+
+
+        input.close();
+    }
+}
